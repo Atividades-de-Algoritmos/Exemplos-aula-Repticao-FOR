080d7799a236ac7556de9d2fa7bfd6191557de60
Carlos <lopes.carlos@academico.ifpb.edu.br>
2022-07-08 16:00:14 -0300

    range() 1 a 9 🔄

--- ex01.py
+++ ex01.py
@@ -1,12 +1,27 @@
 #
 #
-# autores:
+# Autores:
 # Michel Silva
+# Carlos Eduardo
 #
-# data: 26/06/2022
+# Data: 08/07/2022
 #
 # 1 – Imprima de 1 a 9.
-#
-for i in range(1, 10):
-    print(i)
-#
+
+for i in range(1, 10): # Lemos para cada item no tamanho de 1 a 10, no método range() passei dois parâmetros, um de começo "1" e outro de fim "10".
+    print(i) # Imprimindo o item da sequência
+
+print('\nfim do programa') # Informando ao usuário que o programa chegou ao fim
+
+# Versão 2.0 do código
+
+# ------------------------------------------------- #
+
+# By: Carlos Eduardo
+
+# for i in range(1, 10, 1): # Lemos para cada item no tamanho de 1 a 10 de 1 em 1, no método range passando agora três parâmetros, o último parâmetro é a quantidade como será contado.
+#    print(i) # Imprimindo o item da sequência
+
+# print('\nfim do programa') # Informando ao usuário que o programa terminou
+
+# ------------------------------------------------- #
