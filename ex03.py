8102d93ab28d4b4e9b5c589cbe0a8698c6ff73b6
Carlos <lopes.carlos@academico.ifpb.edu.br>
2022-07-08 16:02:35 -0300

    Observando ocorrências 📋

--- ex03.py
+++ ex03.py
@@ -1,27 +1,28 @@
 #
 #
-# autores:
+# Autores:
 # Michel Silva
+# Carlos Eduardo
 #
-# data: 26/06/2022
+# data: 08/06/2022
 #
 # 3 - Faça um algoritmo que imprima a quantidade
 # de ocorrências para cada item contido em uma
-# lista. Exemplo:   ['a', 'b', 'a', 'a', 'b', 'c', 'd', 'c', 'c', 'a', 'b', 'd', 'd', 'a', 'b']
+# lista. Exemplo: ['a', 'b', 'a', 'a', 'b', 'c', 'd', 'c', 'c', 'a', 'b', 'd', 'd', 'a', 'b']
 #
-# entrada de dados:
-lista = ['a', 'b', 'a', 'a', 'b', 'c', 'd', 'c', 'c', 'a', 'b', 'd', 'd', 'a', 'b']
-# processamento:
-print("Lista: ", lista)
-# fazer cast da lista para set para remover os valores repetidos
-conjunto = set(lista) # Cria um conjunto com os elementos da lista (sem repetições)
 
-# processamento e saida de dados:
-for i in set(lista): # Loop que percorre o conjunto e imprime a quantidade de ocorrências de cada elemento
+# Entrada de dados
+
+lista = ['a', 'b', 'a', 'a', 'b', 'c', 'd', 'c', 'c', 'a', 'b', 'd', 'd', 'a', 'b'] # Lista com diversas strings
+
+# Processamento e saída de dados
+
+print(f"Lista: {lista}\n") # Imprimindo a lista completa
+
+conjunto = set(lista) # Utilizando o set() para transformar a lista em um conjunto eliminando todas as repetições
+
+for i in conjunto: # Loop que percorre o conjunto e imprime a quantidade de ocorrências de cada elemento
     contador = lista.count(i) # Conta a quantidade de ocorrências do elemento atual na lista (i)
     print(f"O item '{i}' aparece {contador} vezes") # Imprime a quantidade de ocorrências do elemento atual (i)
 
-print("Fim do programa")
-
-
-
+print("\nfim do programa") # Informando ao usuário que o programa terminou
